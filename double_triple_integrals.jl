d49ad729f5a4a65ca8789b447e00b2063a078563
amel123amel <amelrambabovic@gmail.com>
2023-05-13 18:40:26 +0200

    thi

--- double_triple_integrals.jl
+++ double_triple_integrals.jl
@@ -55,7 +55,38 @@ doubleIntegralWithEdgePoints(f1,2*10^3,2*10^3,0,2,0,pi)
 
 
 
+function doubleIntegralTypeI(f1,n,m,a,b,c,d,y_up, y_do)
 
+
+    delta_X = (b-a)/n
+    delta_Y = (d-c)/m
+    x_f = a+delta_X:delta_X:b; 
+    y_f = c+delta_Y:delta_Y:d; 
+
+    y_upper = y_up; 
+    y_down = y_do; 
+
+    f = f1; # function 
+    V = 0;  # volume 
+
+    for j in 1:length(y_f)
+
+        for i = 1:length(x_f) 
+            if((y_f[j]<=y_upper(x_f[i])) && (y_f[j]>= y_down(x_f[i])))
+               V = V+f(x_f[i],y_f[j])*delta_X*delta_Y;
+            end 
+        end 
+    end 
+
+        display(V); 
+
+end 
+
+y1(x) = 1+x^2
+y2(x) = 2*x^2
+f1(x,y) = x+2*y
+
+doubleIntegralTypeI(f1,2*10^3,2*10^3,-1,1,0,10,y1,y2)
 
 
 
